498e6ce183a28afcab005a4aad2f2269062c6918
amity808 <bolarinwamuhdsodiq0@gmail.com>
2023-12-01 12:09:24 +0100

    fix bug

--- app/car/[id]/page.js
+++ app/car/[id]/page.js
@@ -1,9 +1,7 @@
 'use client'
 import React from "react";
 import Navbar from "@/app/components/Navbar";
-import RentCarModal from "@/app/components/modals/RentCarModal";
 import CarDetails from "@/app/components/CarDetails";
-import RentHistory from "@/app/components/card/RentHistory";
 import HireTrnsactions from "@/app/components/listcard/HireTrnsactions";
 
 const CarBook = ({ params }) => {
