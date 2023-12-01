d8e63739407559cb72d0d3995c9dd8fca091fc72
amity808 <bolarinwamuhdsodiq0@gmail.com>
2023-12-01 19:46:47 +0100

    fix bug

--- app/car/loading.jsx
+++ app/car/loading.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import LoadingIcon from '@/app/components/LoadingIcon';
 const Loading = () => {
   return (
-    <div className=' flex justify-center items-center bg-[#2e37ba] h-screen'>
+    <div className=' flex justify-center items-center bg-[#1E002B] h-screen'>
       <LoadingIcon />
     </div>
   )
